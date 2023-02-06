23d1356d5736b59304e12c506ad7adadab6ea6e3
pleonidou01 <80698205+pleonidou01@users.noreply.github.com>
2023-02-06 21:34:25 +0200

    Update run_multiple_times.sh

--- run_multiple_times.sh
+++ run_multiple_times.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+#run the FL simulation multiple times to present aggregated test evaluation performance metrics
 for i in 1 2 3
 do
    	mkdir "rep$i"
-	python test.py $i
+	python FL_script.py $i
 	
 done
 
